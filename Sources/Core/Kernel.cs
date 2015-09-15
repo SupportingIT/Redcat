86f91cf0ed1742bad781fc994f3d528fd83dbf7e
buzzx8 <buzz-x8@yandex.ru>
2015-09-15 21:01:13 +0300

    Added Kernel

--- Sources/Core/Kernel.cs
+++ Sources/Core/Kernel.cs
@@ -1,6 +1,73 @@
-﻿namespace Redcat.Core
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Redcat.Core
 {
     public class Kernel
-    {  
+    {
+        private List<IServiceProvider> providers = new List<IServiceProvider>();
+        private EventHandlerCollection handlers = new EventHandlerCollection();
+
+        public void AddServiceProvider(IServiceProvider provider)
+        {
+            providers.Add(provider);
+        }
+
+        public T GetService<T>() where T : class
+        {
+            //T service = null;
+            //providers.FirstOrDefault(p => (service = p.GetService(typeof(T)) as T) != null);
+            return providers.Select(p => p.GetService(typeof(T)) as T).FirstOrDefault(s => s != null);                        
+        }
+
+        public IEnumerable<T> GetServices<T>() where T : class
+        {
+            return providers.Select(p => p.GetService(typeof(T)) as T).Where(s => s != null);
+        }
+
+        public void AddEventHandler<T>(string eventId, Action<T> handler) where T : EventArgs
+        {
+            handlers.Add(eventId, handler);
+        }
+
+        public void RemoveEventHandler<T>(string eventId, Action<T> handler) where T : EventArgs
+        {
+            handlers.Remove(eventId, handler);
+        }
+
+        public void RiseEvent<T>(string eventId, T args) where T : EventArgs
+        {            
+            foreach (var handler in handlers.Get<T>(eventId)) handler(args);
+        }
+    }
+
+    internal class EventHandlerCollection
+    {
+        private IDictionary<string, ICollection<object>> handlers = new Dictionary<string, ICollection<object>>();
+
+        public void Add<T>(string eventId, Action<T> handler)
+        {
+            if (!handlers.ContainsKey(eventId)) handlers[eventId] = CreateCollection();
+            handlers[eventId].Add(handler);
+        }
+
+        private ICollection<object> CreateCollection()
+        {
+            return new List<object>();
+        }
+
+        public void Remove<T>(string eventId, Action<T> handler)
+        {
+            if (!handlers.ContainsKey(eventId)) return;
+            handlers[eventId].Remove(handler);
+            if (handlers[eventId].Count == 0) handlers.Remove(eventId);
+        }
+
+        public IEnumerable<Action<T>> Get<T>(string eventId)
+        {
+            if (!handlers.ContainsKey(eventId)) return Enumerable.Empty<Action<T>>();
+            return handlers[eventId].OfType<Action<T>>();
+        }
     }
 }
--- Tests/Core/KernelTests.cs
+++ Tests/Core/KernelTests.cs
@@ -1,9 +1,92 @@
-﻿using NUnit.Framework;
+﻿using FakeItEasy;
+using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace Redcat.Core.Tests
 {
     [TestFixture]
     public class KernelTests
     {
+        [Test]
+        public void GetService_Returns_Service_Provided_By_ServiceProvider()
+        {
+            Kernel kernel = new Kernel();
+            string service = "my-service";
+            IServiceProvider provider = A.Fake<IServiceProvider>();
+            A.CallTo(() => provider.GetService(typeof(string))).Returns(service);
+            kernel.AddServiceProvider(provider);
+
+            string actualService = kernel.GetService<string>();
+
+            Assert.That(ReferenceEquals(actualService, service), Is.True);
+        }
+
+        [Test]
+        public void GetService_Returns_Null_If_No_Providers_For_Service()
+        {
+            Kernel kernel = new Kernel();
+            IServiceProvider provider = A.Fake<IServiceProvider>();
+            kernel.AddServiceProvider(provider);
+
+            Uri actualService = kernel.GetService<Uri>();
+
+            Assert.That(actualService, Is.Null);
+        }
+
+        [Test]
+        public void GetServices_Returns_Services_Provided_By_All_ServiceProviders()
+        {
+            var providers = A.CollectionOfFake<IServiceProvider>(3);
+            A.CallTo(() => providers[0].GetService(typeof(string))).Returns("service0");
+            A.CallTo(() => providers[1].GetService(typeof(string))).Returns(null);
+            A.CallTo(() => providers[2].GetService(typeof(string))).Returns("service2");
+            Kernel kernel = new Kernel();
+            foreach (var provider in providers) kernel.AddServiceProvider(provider);
+
+            var actualServices = kernel.GetServices<string>().ToArray();
+
+            Assert.That(actualServices.Length, Is.EqualTo(2));
+            Assert.That(actualServices.All(s => s != null));
+        }
+
+        [Test]
+        public void RiseEvent_Calls_Added_Event_Handler()
+        {
+            Kernel kernel = new Kernel();
+            bool handlerWasCalled = false;
+            kernel.AddEventHandler<EventArgs>("my-event", args => handlerWasCalled = true);
+
+            kernel.RiseEvent("my-event", EventArgs.Empty);
+
+            Assert.That(handlerWasCalled, Is.True);
+        }
+
+        [Test]
+        public void RiseEvent_Calls_Event_Handler_With_Correct_EventArgs_Type()
+        {
+            Kernel kernel = new Kernel();
+            bool handlerWasCalled = false;
+            kernel.AddEventHandler<EventArgs>("my-event", args => handlerWasCalled = true);
+            kernel.AddEventHandler<AssemblyLoadEventArgs>("my-event", args => handlerWasCalled = false);
+
+            kernel.RiseEvent("my-event", EventArgs.Empty);
+
+            Assert.That(handlerWasCalled, Is.True);
+        }
+
+        [Test]
+        public void RiseEvent_Dont_Calls_Removed_Event_Handler()
+        {
+            Kernel kernel = new Kernel();
+            bool handlerWasCalled = false;
+            Action<EventArgs> handler = args => handlerWasCalled = true;            
+
+            kernel.AddEventHandler("my-event", handler);
+            kernel.RemoveEventHandler("my-event", handler);
+            kernel.RiseEvent("my-event", EventArgs.Empty);
+
+            Assert.That(handlerWasCalled, Is.False);
+        }
     }
 }
