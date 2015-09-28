e85cbf879507faac16be1b15a8e11771794d16d2
BuzzX8 <buzz-x8@yandex.ru>
2015-09-28 18:53:43 +0300

    Added more tests for lexer

--- Sources/Xmpp/Parsing/XmlLexer.cs
+++ Sources/Xmpp/Parsing/XmlLexer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -6,24 +7,31 @@ namespace Redcat.Xmpp.Parsing
 {
     public static class XmlLexer
     {
-        public static readonly string XmlTagRegex = @"\<\w[\s|\S]*/\>";
-        public static readonly string XmlStartTagRegex = @"\<\w+\>";
-        public static readonly string XmlClosingTagRegex = @"\</\w+\>";
-        public static readonly string XmlValueRegExp = @"[^\<\>/]+";
-        public static readonly string XmlNodeRegExp = string.Format(@"({0})|([^\<\>/]+)|({1})", XmlStartTagRegex, XmlClosingTagRegex);
+        public static readonly string XmlEnclosedTagRegex = @"\<\w[\s|\S]*\/\>";
+        public static readonly string XmlStartTagRegex = @"\<\w+[\s|\S]*\>";
+        public static readonly string XmlClosingTagRegex = @"\<\/\w+\>";
+        public static readonly string XmlValueRegex = @"[^\<\>\/]+";
+        public static readonly string XmlElementWithValueRegex = string.Format(@"({0}\b)|({1})|({2})", XmlStartTagRegex, XmlValueRegex, XmlClosingTagRegex);
+        public static readonly string GenericXmlElementRegex = string.Format("({0})|({1})", XmlEnclosedTagRegex, XmlElementWithValueRegex);
 
         public static IEnumerable<XmlToken> GetTokens(string xmlFragment)
         {
-            var tokens = Regex.Matches(xmlFragment, XmlNodeRegExp);
+            var tokens = Regex.Matches(xmlFragment, GenericXmlElementRegex);
             return tokens.Cast<Match>().Select(m => new XmlToken(m.Value, GetTokenType(m.Value)));
+        }
+
+        public static string GetTagName(XmlToken token)
+        {
+            var match = Regex.Match(token.Text, @"\<\w+").Value;
+            return match.Substring(1, match.Length - 1);
         }
 
         private static XmlTokenType GetTokenType(string token)
         {
+            if (Regex.IsMatch(token, XmlEnclosedTagRegex)) return XmlTokenType.EnclosedTag;
             if (Regex.IsMatch(token, XmlStartTagRegex)) return XmlTokenType.StartTag;
-            if (Regex.IsMatch(token, XmlValueRegExp)) return XmlTokenType.Value;
             if (Regex.IsMatch(token, XmlClosingTagRegex)) return XmlTokenType.ClosingTag;
-            return XmlTokenType.Tag;
+            return XmlTokenType.Value;
         }
     }
 
@@ -51,9 +59,9 @@ namespace Redcat.Xmpp.Parsing
 
     public enum XmlTokenType
     {
+        EnclosedTag,
         StartTag,
         ClosingTag,
-        Tag,
         Value
     }
 }
--- Tests/Xmpp/Parsing/XmlLexerTests.cs
+++ Tests/Xmpp/Parsing/XmlLexerTests.cs
@@ -8,28 +8,55 @@ namespace Redcat.Xmpp.Tests.Parsing
     public class XmlLexerTests
     {
         [Test]
-        public void GetTokens_Correctly_Parses_Single_Tag()
+        public void GetTokens_Correctly_Parses_Enclosed_Tag()
         {
-            string element = "<element />";
+            VerifyEnclosingTagParsing("<element />");
+        }
 
-            var tokens = XmlLexer.GetTokens(element);
+        [Test]
+        public void GetTokens_Correctly_Parses_Enclosed_Tag_With_Attributes()
+        {
+            VerifyEnclosingTagParsing("<element attr1='val0' attr2='val' />");
+        }
 
-            Assert.That(tokens.Count(), Is.EqualTo(1));
-            Assert.That(tokens.ElementAt(0).Type, Is.EqualTo(XmlTokenType.Tag));
+        private void VerifyEnclosingTagParsing(string tag)
+        {
+            var tokens = XmlLexer.GetTokens(tag).ToArray();
+            Assert.That(tokens.Length, Is.EqualTo(1));
+            Assert.That(tokens[0].Type, Is.EqualTo(XmlTokenType.EnclosedTag));
         }
 
         [Test]
         public void GetTokens_Correctly_Parses_Element_With_Value()
         {
-            string element = "<node>Value</node>";
+            VerifyElementWithValueParsing("<node>Value</node>");
+        }
 
+        [Test]
+        public void GetTokens_Correctly_Parses_Element_With_Attributes_And_Value()
+        {
+            VerifyElementWithValueParsing("<node attr1='val0' attr2='val'>Value</node>");
+        }
+
+        private void VerifyElementWithValueParsing(string element)
+        {
             var tokens = XmlLexer.GetTokens(element).ToArray();
 
-            //Assert.That(tokens.Length, Is.EqualTo(3));
-            //Assert.That(tokens[0].Type, Is.EqualTo(XmlTokenType.StartTag));
-            //Assert.That(tokens[1].Type, Is.EqualTo(XmlTokenType.Value));
-            //Assert.That(tokens[1].Text, Is.EqualTo("Value"));
+            Assert.That(tokens.Length, Is.EqualTo(3));
+            Assert.That(tokens[0].Type, Is.EqualTo(XmlTokenType.StartTag));
+            Assert.That(tokens[1].Type, Is.EqualTo(XmlTokenType.Value));
+            Assert.That(tokens[1].Text, Is.EqualTo("Value"));
             Assert.That(tokens[2].Type, Is.EqualTo(XmlTokenType.ClosingTag));
+        }
+
+        [Test]
+        public void GetTagName_Returns_Tag_Name([Values(@"<kitty />", @"<kitty>", @"<kitty attr1='val1' attr0='val0'>")]string tag)
+        {
+            XmlToken token = XmlLexer.GetTokens(tag).Single();
+
+            string name = XmlLexer.GetTagName(token);
+
+            Assert.That(name, Is.EqualTo("kitty"));
         }
     }
 }
