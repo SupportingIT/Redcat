3228e1f4c040fb7be05f63c9fd731e2650d47051
buzzx8 <buzz-x8@yandex.ru>
2015-10-24 20:58:41 +0300

    added extensions for stream features

---
+++ Sources/Xmpp/Xml/StreamFeatureExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Redcat.Xmpp.Xml
+{
+    public static class StreamFeatureExtensions
+    {
+        public static bool HasTlsFeature(this IEnumerable<XmlElement> features)
+        {
+            return features.HasElement("starttls", Namespaces.Tls);
+        }
+
+        public static bool HasTlsFeature(this XmlElement element)
+        {
+            return HasTlsFeature(element.Childs);
+        }
+
+        public static XmlElement TlsFeature(this IEnumerable<XmlElement> features)
+        {
+            return features.Element("starttls", Namespaces.Tls);
+        }
+
+        public static XmlElement TlsFeature(this XmlElement element)
+        {
+            return TlsFeature(element.Childs);
+        }
+    }
+}
---
+++ Tests/Xmpp/Xml/StreamFeatureExtensionTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using Redcat.Xmpp.Xml;
+
+namespace Redcat.Xmpp.Tests.Xml
+{
+    [TestFixture]
+    public class StreamFeatureExtensionTests
+    {
+        [Test]
+        public void HasTlsFeature_Returns_False_If_No_Tls_Feature()
+        {
+            var elements = new[] { new XmlElement("e1"), new XmlElement("e2") };
+
+            bool hasFeature = elements.HasTlsFeature();
+
+            Assert.That(hasFeature, Is.False);
+        }
+
+        [Test]
+        public void  HasTlsFeature_Returns_True_If_Sequence_Contains_Tls_Feature()
+        {
+            var elements = new[] { new XmlElement("e34"), Tls.Start, new XmlElement("j43") };
+
+            bool hasFeature = elements.HasTlsFeature();
+
+            Assert.That(hasFeature, Is.True);
+        }
+
+        [Test]
+        public void TlsFeature_Returns_Null_If_No_Tls_Feature()
+        {
+            var elements = new[] { new XmlElement("e1"), new XmlElement("e2") };
+
+            var feature = elements.TlsFeature();
+
+            Assert.That(feature, Is.Null);
+        }
+
+        [Test]
+        public void TlsFeature_Returns_Tls_Feature_If_sequence_Contains_It()
+        {
+            var elements = new[] { new XmlElement("e34"), Tls.Start, new XmlElement("j43") };
+
+            var feature = elements.TlsFeature();
+
+            Assert.That(feature.Name, Is.EqualTo("starttls"));
+            Assert.That(feature.Xmlns, Is.EqualTo(Namespaces.Tls));
+        }
+    }
+}
