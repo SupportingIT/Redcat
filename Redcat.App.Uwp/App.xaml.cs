f37822149d32bd05bb2f00cb2dbe83764ac57843
BuzzX8 <buzz-x8@yandex.ru>
2016-03-24 00:00:58 +0200

    Added UWP

---
+++ Redcat.App.Uwp/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using Redcat.App.Uwp.Views;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+
+namespace Redcat.App.Uwp
+{
+    sealed partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Suspending += OnSuspending;
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+            var homeView = new HomeView();
+            Setup setup = new Setup(homeView.Frame);
+            setup.Initialize();
+
+            var start = Mvx.Resolve<IMvxAppStart>();
+            start.Start();
+            
+            Window.Current.Activate();
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();            
+            deferral.Complete();
+        }
+    }
+}
---
+++ Redcat.App.Uwp/Views/HomeView.xaml.cs
@@ -0,0 +1,10 @@
+﻿using MvvmCross.WindowsUWP.Views;
+
+namespace Redcat.App.Uwp.Views
+{
+    public partial class HomeView : MvxWindowsPage
+    {
+        public HomeView()
+        { }
+    }
+}
