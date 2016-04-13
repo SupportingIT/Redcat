667c0101c8a18ad1ba5781e5e53539fe47aae083
m.chasnyk <mchasnyk@avid.com>
2016-04-13 23:36:03 +0300

    Missed files

---
+++ Tests/Redcat.Core/Serialization/StreamExtensionTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using Redcat.Core.Serialization;
+using Redcat.Test;
+using System;
+using System.IO;
+
+namespace Redcat.Core.Tests.Serialization
+{
+    [TestFixture]
+    public class StreamExtensionTests
+    {
+        private MemoryStream stream;
+
+        [SetUp]
+        public void SetUp()
+        {
+            stream = new MemoryStream();
+        }
+
+        short[] WriteInt16TestData = { short.MinValue, 0, short.MaxValue };
+
+        [Test]
+        public void Write_Correctly_Serializes_Int16_Value([ValueSource(nameof(WriteInt16TestData))]short value)
+        {
+            VerifyWriteMethod(value.ToString("x4"), s => s.Write(value));
+        }
+
+        ushort[] WriteUInt16TestData = { 0, ushort.MaxValue / 2, ushort.MaxValue };
+
+        [Test]
+        public void Write_Correctly_Serializes_UInt16([ValueSource(nameof(WriteUInt16TestData))]ushort value)
+        {
+            VerifyWriteMethod(value.ToString("x4"), s => s.Write(value));
+        }
+
+        int[] WriteInt32TestData = { int.MinValue, 0, int.MaxValue };
+
+        [Test]
+        public void Write_Correctly_Serializes_Int32([ValueSource(nameof(WriteInt32TestData))]int value)
+        {
+            VerifyWriteMethod(value.ToString("x8"), s => s.Write(value));
+        }
+
+        uint[] WriteUInt32TestData = { 0, uint.MaxValue / 2, uint.MaxValue };
+
+        [Test]
+        public void Write_Correctly_Serializes_UInt32_Value([ValueSource(nameof(WriteUInt32TestData))]uint value)
+        {
+            VerifyWriteMethod(value.ToString("x8"), s => s.Write(value));
+        }
+
+        long[] WriteInt64TestData = { long.MinValue, 0, long.MaxValue };
+
+        [Test]
+        public void Write_Correctly_Serializes_Int64_Value([ValueSource(nameof(WriteInt64TestData))]long value)
+        {
+            VerifyWriteMethod(value.ToString("x16"), s => s.Write(value));
+        }
+
+        ulong[] WriteUInt64TestData = { 0, ulong.MaxValue / 2, ulong.MaxValue };
+
+        [Test]
+        public void Write_Correctly_Serializes_UInt64([ValueSource(nameof(WriteUInt64TestData))]ulong value)
+        {
+            VerifyWriteMethod(value.ToString("x16"), s => s.Write(value));
+        }
+
+        private void VerifyWriteMethod(string hexString, Action<Stream> write)
+        {
+            byte[] expected = BinaryUtils.ToByteArray(hexString);
+
+            write(stream);
+
+            byte[] actual = stream.ToArray();
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tests/Redcat.TestUtils/BinaryUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Redcat.Test
+{
+    public static class BinaryUtils
+    {
+        public static byte[] ToByteArray(string hexString)
+        {
+            List<byte> bytes = new List<byte>();
+
+            for (int i = 0; i < hexString.Length; i += 2)
+            {
+                byte b = byte.Parse(hexString.Substring(i, 2), NumberStyles.HexNumber);
+                bytes.Add(b);
+            }
+
+            return bytes.ToArray();
+        }
+    }
+}
